276bb97b0c4cf015d60ce9c3feac550e1de22839
Xin-Heng <xinheng1993@gmail.com>
2018-04-14 16:16:13 -0700

    update document formate

--- Assignment2/Assignment2/ER.h
+++ Assignment2/Assignment2/ER.h
@@ -16,18 +16,23 @@ class Er : public menu{
 	Category s;
 public:
 	Er() {}
-	
-	virtual void retry_home(bool is_error) override;
 	virtual void home_page() override;
 	virtual string category_page() override;
-	void system_start() {home_page();}
-	void update_queue();
-	void get_non_critical();
-	bool compare_date(Date admin_date, int admin_hr, int admin_min, int promo_min);
-	void promo_cate(erPatient& temp);
-	int get_cate_num(string);
+	virtual void retry_home(bool is_error) override;
+	void back_home(double &zero);
 
-	//input functions
+	//start system
+	void system_start() {home_page();}
+
+	//home page menu
+	void add_patients();
+	void get_next_patient();
+	void change_category();
+	void save_to_file();
+	void load_patients_list();
+	void print_patient();
+
+	//add_patients related function
 	void enter_first_name(string& first);
 	void enter_middle_name(string& middle);
 	void enter_last_name(string& last);
@@ -36,18 +41,23 @@ public:
 	void initialize_admin_time(int& hour, int& minute);
 	void enter_symptoms(string& symptoms);
 	void choose_category(string& category);
-	
-
-	void add_patients();
 	void sort_patients();
-	void get_next_patient();
-	void print_patient();
-	void save_to_file();
-	bool load_file();
-	void load_patients_list();
-	void change_category();
-	void back_home(double &zero);
+
+	//change_category related function
 	void find_patient(int patient_phn);
+
+	//promotion process
+	void update_queue();
+	void get_non_critical();
+	int get_cate_num(string);
+	bool compare_date(Date admin_date, int admin_hr, int admin_min, int promo_min);
+	void promo_cate(erPatient& temp);
+	
+	//load patients list
+	bool load_file();
+
+
+	//check input functions
 	void check_input_days(int& year, int& month, int& days);
 	void check_input_integer(int lower, int upper, int& value);
 	void check_phn(int& phn);
